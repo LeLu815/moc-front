e1dce1b47df8172a276e75fe333e4c038113ee7e
LinQuixote <leein9404@naver.com>
2024-02-29 19:56:24 +0900

    Feat : 디테일페이지, 댓글 로더 작성

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,16 +1,21 @@
-import { json, useLoaderData, useNavigate } from "react-router-dom";
+import {
+  json,
+  useLoaderData,
+  useNavigate,
+  useRouteLoaderData,
+} from "react-router-dom";
 
 import { publicApi } from "../util/http";
 import classes from "./PostPage.module.css";
+import PostComment from "./PostComment";
 
 const postDetails = {
   title: "작성된 게시글의 제목입니다.",
   author: "작성자",
   timestamp: "2023.04.23.",
-  views: 15,
   comments: 10,
   tags: ["#해시태그1", "#해시태그2", "#해시태그3"],
-  imageUrl: "path_to_image.jpg",
+  body: "작성된 게시글의 내용입니다. 수필(隨筆) 또는 경수필(輕隨筆, miscellany)은 일정한 형식을 따르지 않고 가볍게는 일상적인 느낌이나 체험[1]을 생각나는 대로, 자유롭게 쓰는 산문 형식의 문학을 가리킨다. 가벼운 잡기를 자유롭게 서술하는 경수필에 대비하여 사회적 주제 또는 철학적 사색 등을 무거운 논조를 통해 논리적, 객관적으로 서술하는 글은 중수필(重隨筆)이라고 한다.",
 };
 const comments = [
   { id: 1, text: "댓글1" },
@@ -23,9 +28,12 @@ const PostPage = () => {
   const handleErrorClick = () => {
     return navigate(-1);
   };
+  const { user } = useRouteLoaderData("root");
   const loaderData = useLoaderData();
+  const dataObj = new Date(loaderData.data.created_at);
+  const dateFormating = `${dataObj.getFullYear()}-${dataObj.getMonth()}-${dataObj.getDate()}`;
 
-  console.log("loaderData :", loaderData);
+  console.log("loaderData :", loaderData, user);
 
   return (
     <div className={classes.page}>
@@ -43,31 +51,55 @@ const PostPage = () => {
         </div>
       )}
       <div className={classes.postContainer}>
-        <h1>{postDetails.title}</h1>
+        <h1>{loaderData.data ? loaderData.data.title : postDetails.title}</h1>
         <div className={classes.postMeta}>
-          <span>{postDetails.author}</span>
-          <span>{postDetails.timestamp}</span>
-          <span>조회 {postDetails.views}</span>
-          <span>댓글 {postDetails.comments}</span>
+          <div className={classes.offical_postMeta}>
+            <span>
+              {loaderData.data
+                ? `user_${loaderData.data.user}`
+                : postDetails.author}
+            </span>
+            <span>
+              {loaderData.data ? dateFormating : postDetails.timestamp}
+            </span>
+            <span>
+              {loaderData.data
+                ? `댓글 ${loaderData.data.comments.length}`
+                : `댓글 ${postDetails.comments}`}
+            </span>
+          </div>
+          <div className={classes.private_postMeta}>
+            {loaderData.data && loaderData.data.user === user.id && (
+              <>
+                <button type="button">수정</button>
+                <button type="button">삭제</button>
+              </>
+            )}
+          </div>
         </div>
         <div className={classes.postContent}>
-          {postDetails.tags.map((tag) => (
-            <span key={tag}>{tag}</span>
-          ))}
-          <img src={postDetails.imageUrl} alt="Post" />
-        </div>
-        <div className={classes.comments}>
-          {comments.map((comment) => (
-            <div key={comment.id} className="comment">
-              {comment.text}
-            </div>
-          ))}
-        </div>
-        <div className={classes.commentForm}>
-          <input type="text" placeholder="댓글을 작성하세요" />
-          <button>댓글 작성</button>
+          <div className={classes.postContent_hash}>
+            {loaderData.data
+              ? loaderData.data.hashtags.map((tag) => (
+                  <span key={tag.id}>{`#${tag.tags}`}</span>
+                ))
+              : postDetails.tags.map((tag) => <span key={tag}>{tag}</span>)}
+          </div>
+          <div className={classes.postContent_body}>
+            {loaderData.data ? loaderData.data.body : postDetails.body}
+          </div>
+          <div className={classes.postContent_img}>
+            {loaderData.data &&
+              loaderData.data.image.length !== 0 &&
+              loaderData.data.image.map((url) => (
+                <img key={url.id} src={url.image} alt="이미지 사진" />
+              ))}
+          </div>
         </div>
       </div>
+      <PostComment
+        comments={loaderData.data ? loaderData.data.comments : false}
+      />
     </div>
   );
 };
